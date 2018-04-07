d9e385e91e20df529a36f0bed26499d9ebd4550a
Bachynin Ivan <bachynin.i@gmail.com>
2018-04-07 21:15:52 +0300

    🎨📚 refactor and add docs

--- easy_flags/base.py
+++ easy_flags/base.py
@@ -9,57 +9,103 @@ class BaseConfig(object):
     _desc = None
 
     def __init__(self, desc=None, strict=False):
-        self.attrs = [a for a in dir(self)
-                      if not a.startswith('_')
-                      and not callable(getattr(self, a))]
-        self.define_map = {
-            str: self.define_str,
-            int: self.define_int,
-            float: self.define_float,
-            bool: self.define_bool,
+        self._attrs = [a for a in dir(self)
+                       if not a.startswith('_')
+                       and not callable(getattr(self, a))]
+        self._define_map = {
+            str: self._define_str,
+            int: self._define_int,
+            float: self._define_float,
+            bool: self._define_bool,
         }
         conflict_handler = 'error' if strict else 'resolve'
-        self.parser = argparse.ArgumentParser(description=desc or self._desc,
-                                              conflict_handler=conflict_handler)
-        self.args = None
-        self.defined = False
+        self._parser = argparse.ArgumentParser(description=desc or self._desc,
+                                               conflict_handler=conflict_handler)
+        self._args = None
+        self._defined = False
 
-    def call_definer(self, attr: str, typ, value, doc=''):
-        definer = self.define_map.get(typ, None)
+    def define(self):
+        """
+        Parse and define flags. This function should be called before using config object.
+        """
+        if self._defined:
+            return
+        for attr in self._attrs:
+            value = getattr(self, attr)
+            typ = type(value)
+            if typ is tuple or typ is list:
+                self._manage_tuple(attr, value)
+            else:
+                self._call_definer(attr, typ, value)
+
+        self._args = self._parser.parse_args()
+        self.fill_attributes(self)
+        self._defined = True
+
+    def fill_attributes(self, obj):
+        """
+        Fill `obj` with defined attributes.
+        """
+        if not self._defined:
+            self.define()
+        for a in self._attrs:
+            setattr(obj, a, getattr(self._args, a))
+
+    def print(self, title=None, block_size=39, prefix='|  '):
+        """
+        Pretty-print all flags.
+        """
+        if not self._defined:
+            self.define()
+        max_len = max([len(a) for a in self._attrs])
+        print('+ ' + '- ' * block_size)
+        if title:
+            print(prefix + title)
+        for a in self._attrs:
+            key = a.ljust(max_len)
+            value = getattr(self, a)
+            print(f'{prefix}{key} : {value}')
+        print('+ ' + '- ' * block_size)
+
+    def _call_definer(self, attr: str, typ, value, doc=''):
+        definer = self._define_map.get(typ, None)
         if definer is None:
             raise ConfigurationError("Unknown type for field '{0}'. "
                                      "Should be primitive type.".format(attr))
         definer(attr, value, doc)
 
-    def define_str(self, attr, value, doc=None):
-        doc = doc or 'string value'
-        self.define_arg(attr, str, value, doc)
+    def _define_str(self, attr, value, doc=None):
+        doc = doc or 'string field'
+        self._define_arg(attr, str, value, doc)
 
-    def define_int(self, attr, value, doc=None):
-        doc = doc or 'int value'
-        self.define_arg(attr, int, value, doc)
+    def _define_int(self, attr, value, doc=None):
+        doc = doc or 'int field'
+        self._define_arg(attr, int, value, doc)
 
-    def define_float(self, attr, value, doc=None):
-        doc = doc or 'float value'
-        self.define_arg(attr, float, value, doc)
+    def _define_float(self, attr, value, doc=None):
+        doc = doc or 'float field'
+        self._define_arg(attr, float, value, doc)
 
-    def define_bool(self, attr, value, doc=None):
+    def _define_bool(self, attr, value, doc=None):
         doc = doc or 'boolean flag'
         action = 'store_false' if value else 'store_true'
-        self.define_arg(attr, str, value, doc, action=action)
+        self._define_arg(attr, str, value, doc, action=action)
 
-    def manage_tuple(self, attr, value):
+    def _manage_tuple(self, attr, value):
+        err_msg = "Bad definition for field '{0}'. Tuple must have one of the next formats: " \
+                  "(default_value, docstring) or (default_value, type, docstring)".format(attr)
         if len(value) == 2:
             value, doc = value
             typ = type(value)
         elif len(value) == 3:
             value, typ, doc = value
         else:
-            raise ConfigurationError("Improper configuration for field '{0}'. Config field should be "
-                                     "primitive or tuple with length 2 or 3.".format(attr))
-        self.call_definer(attr, typ, value, doc)
+            raise ConfigurationError(err_msg)
+        if type(typ) is not type or type(doc) is not str:
+            raise ConfigurationError(err_msg)
+        self._call_definer(attr, typ, value, doc)
 
-    def define_arg(self, attr: str, typ, value, doc='', action=None):
+    def _define_arg(self, attr: str, typ, value, doc='', action=None):
         params = {
             'dest': attr,
             'type': typ,
@@ -70,39 +116,7 @@ class BaseConfig(object):
         if action and action.startswith('store'):
             params.pop('type')
         dash = '-' * min(2, len(attr))
-        self.parser.add_argument(dash + attr, **params)
-
-    def define(self):
-        if self.defined:
-            return
-        for attr in self.attrs:
-            value = getattr(self, attr)
-            typ = type(value)
-            if typ is tuple or typ is list:
-                self.manage_tuple(attr, value)
-            else:
-                self.call_definer(attr, typ, value)
-
-        self.args = self.parser.parse_args()
-        self.fill_attributes(self)
-        self.defined = True
-
-    def fill_attributes(self, obj):
-        for a in self.attrs:
-            setattr(obj, a, getattr(self.args, a))
-
-    def print(self, title=None, block_size=39, prefix='|  '):
-        if not self.defined:
-            self.define()
-        max_len = max([len(a) for a in self.attrs])
-        print('+ ' + '- ' * block_size)
-        if title:
-            print(prefix + title)
-        for a in self.attrs:
-            key = a.ljust(max_len)
-            value = getattr(self, a)
-            print(f'{prefix}{key} : {value}')
-        print('+ ' + '- ' * block_size)
+        self._parser.add_argument(dash + attr, **params)
 
 
 class ExampleConfig(BaseConfig):
